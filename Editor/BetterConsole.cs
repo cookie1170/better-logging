421605c4fad422910d76d14b8a95905a4a0c8f91
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2025-11-04 18:59:20 +1000

    Make the console use a ListView

--- Editor/BetterConsole.cs
+++ Editor/BetterConsole.cs
@@ -1,12 +1,12 @@
 using Cookie.BetterLogging.Serialization;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Cookie.BetterLogging.Editor
 {
     public class BetterConsole : EditorWindow
     {
+        private ListView _currentEntriesContainer;
         private StyleSheet _styleSheet;
 
         private void OnEnable() {
@@ -35,17 +35,42 @@ namespace Cookie.BetterLogging.Editor
 
             topBar.Add(clearButton);
 
-            ScrollView entriesContainer = new();
-            entriesContainer.scrollOffset = new Vector2(0, entriesContainer.verticalScroller.highValue);
-            entriesContainer.AddToClassList("entries");
+            _currentEntriesContainer = new ListView {
+                bindItem = BindEntry,
+                makeItem = MakeEntry,
+                itemsSource = BetterLog.Logs,
+                virtualizationMethod = CollectionVirtualizationMethod.DynamicHeight,
+                showAlternatingRowBackgrounds = AlternatingRowBackground.All,
+                makeNoneElement = () => null,
+            };
 
-            for (int i = 0; i < BetterLog.Logs.Count; i++) {
-                LogEntry log = BetterLog.Logs[i];
-                entriesContainer.Add(GetEntryFor(log, i % 2 == 0));
-            }
+            _currentEntriesContainer.AddToClassList("entries");
 
             rootVisualElement.Add(topBar);
-            rootVisualElement.Add(entriesContainer);
+            rootVisualElement.Add(_currentEntriesContainer);
+        }
+
+        private static VisualElement MakeEntry() {
+            VisualElement item = new();
+            VisualElement content = new();
+
+            Label contentLabel = new() { name = "content" };
+            Label traceLabel = new() { name = "stackTrace" };
+
+            content.Add(contentLabel);
+            content.Add(traceLabel);
+            item.Add(content);
+            item.AddToClassList("entry");
+            content.AddToClassList("entry-content");
+
+            return item;
+        }
+
+        private static void BindEntry(VisualElement element, int index) {
+            LogEntry entry = BetterLog.Logs[index];
+
+            element.Q<Label>("content").text = $"[{entry.Time}] {Serializer.Serialize(entry.Content)}";
+            element.Q<Label>("stackTrace").text = entry.StackTrace;
         }
 
         private void Clear() {
@@ -58,20 +83,10 @@ namespace Cookie.BetterLogging.Editor
         }
 
         private void Redraw() {
-            rootVisualElement.Clear();
-            CreateGUI();
-            Repaint();
-        }
+            if (_currentEntriesContainer == null) return;
 
-        private static VisualElement GetEntryFor(LogEntry entry, bool isAlternate) {
-            VisualElement container = new();
-            container.AddToClassList("entry");
-            container.AddToClassList(isAlternate ? "entry-alternate" : "entry-normal");
-
-            container.Add(new Label($"[{entry.Time}] {Serializer.Serialize(entry.Content)}"));
-            container.Add(new Label(entry.StackTrace));
-
-            return container;
+            _currentEntriesContainer.RefreshItems();
+            if (BetterLog.Logs.Count > 0) _currentEntriesContainer.ScrollToItem(BetterLog.Logs.Count - 1);
         }
 
         [MenuItem("Window/Cookie/Better Console")]
